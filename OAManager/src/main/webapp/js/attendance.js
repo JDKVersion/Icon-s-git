e6c9b80b50a539303ed02b9d93451bb9d7913ad4
icon <2647706577@qq.com>
2019-03-08 12:42:53 +0800

    考勤管理修改，信息管理修改

--- OAManager/src/main/webapp/js/attendance.js
+++ OAManager/src/main/webapp/js/attendance.js
@@ -13,7 +13,6 @@ var setting = {
 var zNode1 =[
     { id:1, pId:0, name:"考勤管理"},
     { id:2, pId:1, name:"考勤统计"},
-    { id:3, pId:1, name:"加班申请"}
 ]
 
 /* 初始化各个功能的树*/
--- OAManager/src/main/webapp/js/datum.js
+++ OAManager/src/main/webapp/js/datum.js
@@ -13,7 +13,6 @@ var setting = {
 
 var zNode0=[
     { id:1, pId:0, name:"资料管理"},
-    { id:2, pId:1, name:"资料信息"},
 ];
 
 
